12e549c3ccb58375c955ceb88c0c328a9049b29a
unknown <axmedobsiye15@gmail.com>
2023-08-01 07:41:25 +0300

    adding google-auth2.0 and passportjs

--- server.js
+++ server.js
@@ -3,12 +3,35 @@ const https = require('https');
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+require('dotenv').config();
 
 const PORT = 3000;
 
+const config = {
+	CLIENT_ID: process.env.CLIENT_ID,
+	CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
 const app = express();
 app.use(helmet());
-app.get('/secret', (req, res) => {
+
+function CheckLoggedIn(req, res, next) {
+	const isLoggedIn = true // FUNCTION that prevents unauthorized users to access secret endpoint 
+	if (!isLoggedIn) {
+		return res.status(401).json({
+			error: 'You must log in!',
+		});
+	}
+	next();
+}
+
+app.get('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', CheckLoggedIn, (req, res) => {
 	return res.send('Your personal secret value is 42!');
 });
 
